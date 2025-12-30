567e25d591aca1d93122d041bd2f9eea93051b91
Piotr Tutak <p.tutak@outlook.com>
2025-12-31 00:00:19 +0100

    init

--- src/coreiesuai/settings.py
+++ src/coreiesuai/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=4c$6ev(vk^@%ss@$mqv=0n8(61upep+&p^2oh@gb)9#($%!4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
